f1bbcf31394493f55d420037501edde24a3cedee
Sebastian <luszczeksebastian@gmail.com>
2019-05-23 00:24:48 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin')
+
+const app = express();
+
+// middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+
+app.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+app.listen(3000, () => {
+    console.log('Server started...')
+});
